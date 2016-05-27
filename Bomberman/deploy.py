e62665d915d106e9042fb18f0bf6382e03979a8c
Giulio  Jiang <giulio.jiang14@imperial.ac.uk>
2016-05-27 11:55:16 +0100

    added bomberman deploy script

---
+++ Bomberman/deploy.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import subprocess
+import sys
+import colors
+import random
+import os
+import argparse
+
+def main(argv=None):
+    
+    git_source = 'https://gitlab.doc.ic.ac.uk/WebApps-Group-12/BomberMan.git'
+    cloned_directory = 'out'
+    
+    # .........................................................................
+    # load command line arguments
+    if argv is None:
+        argv = sys.argv[1:]
+
+    # .........................................................................
+    # parse command line arguments (argparse)
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--output',
+                        help='Output directory',
+                        default='/var/www/'
+                        )
+
+    args = parser.parse_args(argv)
+
+    
+    
+    # .........................................................................
+    # Clean out directory
+    subprocess.call(['rm', '-rf', cloned_directory])
+    
+    # .........................................................................
+    # Clone from git
+    subprocess.call(['git', 'clone', git_source, cloned_directory, '--depth', '1'])
+    
+    # .........................................................................
+    # Move client to deploy folder
+    subprocess.call(['mv', cloned_directory + os.sep + 'client', args.output + os.sep + 'BOMBERMAN'])
+    
+    # .........................................................................
+    # Overwrite the address file
+    filename = args.output + os.sep + 'BOMBERMAN' + os.sep + 'server_url.js'
+    content = "socketURL = 'ws://webapps.jstudios.ovh:1080';"
+    
+    # .........................................................................
+    # Compile the server
+    os.chdir(cloned_directory + os.sep + server);
+    subprocess.call(['make']);
+    
+# ==============================================================================
+# call main if executed as script
+if __name__ == '__main__':
+    sys.exit(main())
